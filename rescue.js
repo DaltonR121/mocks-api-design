5736ac29acfaa783a4a83d49630df61f34d4dee0
Ryan <DaltonR121@gmail.com>
2021-11-02 10:48:32 -0400

    added rescue design

---
+++ rescue.js
@@ -0,0 +1,23 @@
+// Create a structure to hold an "Animal Rescue".  Our animal rescue
+// should have dogs, cats, and snakes (insert Python joke here...?).
+// Each from animal type, we should then have specific breeds for each
+// animal.  After breeds, we need a name for each animal, as well as a status
+// which should be "Available", "Pending", or "Adopted".
+
+let rescue = {
+};
+
+const getAvailableDogs = (rescue) => {
+  // From the structure we created, return an array with names
+  // of dogs available for adoption.
+};
+
+const getPendingCats = (rescue) => {
+  // From the structure we created, return an array with names
+  // of cats pending adoption.
+}
+
+const getAllAdoptions = (rescue) => {
+  // From the structure we created, return an array with names
+  // of all animals that have been adopted.
+}
